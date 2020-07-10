075f29eb0c29fbd9cc76dbe0fa51e95151972ff8
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-07-10 16:52:32 +0300

    #all done+gtm

--- js/fader.js
+++ js/fader.js
@@ -1,5 +1,14 @@
 $(document).ready( () => {
-    $('#monitor-logo').fadeIn(2000);
-    $('#monitor-title').fadeIn(5000)
-
+    $('#monitor-logo').fadeIn(1500);
+    $('#monitor-title').fadeIn(2500)
 })
+
+$(window).scroll(function() {
+    $('.product-item').each(function(){
+      var imagePos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+470) {
+        $(this).css('opacity', 1);
+      }
+    });
+});
